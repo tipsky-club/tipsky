f0cf36bdb91a02d1df173a207d9b7410900678bf
Maximilian Wiens <mwiens@piggy.CeBiTec.Uni-Bielefeld.DE>
2017-06-23 14:14:36 +0200

    added request support

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    heim = request.args.get('heim')
+    gast = request.args.get('gast')
+    return 'Es spielt ' + heim + " gegen " + gast
