245a46118497df843b216abfcf8a1e8a87ea7321
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2017-07-21 15:57:30 +0200

    Added a file for the naive algorithm.

---
+++ naivAlgo.py
@@ -0,0 +1,16 @@
+import random
+
+'''
+This file contains the first naive algorithm.
+
+The algorithm consists of two phases. In the first phase, the outcome of a match is estimated. This happens in three
+steps. The first step considers the achievements of both teams in the current season up to now. This is done by
+comparing their rank in the table. (...) In the second
+phase, the result of the match is guessed.
+'''
+
+if __name__ == '__main__':
+    #We need two teams that play against each other (Later the names of the teams are given as an input to the program.
+    #For now we define them here) TODO: Change this!
+    hTeam = "FC Bayern"
+    gTeam = "Borussia Dortmund"
