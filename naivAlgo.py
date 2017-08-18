1930119494ab49b3ba727a8599fdfd9a4b66e9eb
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2017-08-18 19:02:28 +0200

    Little progress in with naive algorithm.

--- naivAlgo.py
+++ naivAlgo.py
@@ -71,53 +71,134 @@ def compare_runs(h_matches, g_matches):
 def guessResult(h_matches, g_matches, outcome):
     possibleResults = {}
 
-    #Depending on the predicted outcome diffent results are possible
-    if outcome == 0:
-        possibleResults = {(0, 0): 0.0, (1, 1): 0.0, (2, 2): 0.0}
-    else:
-        possibleResults = {(1, 0): 0.0, (2, 0): 0.0, (2, 1): 0.0, (3, 0): 0.0, (3, 1): 0.0, (3, 2): 0.0}
-
     h_avg_shot_goals = (h_matches[0][0] + h_matches[1][0] + h_matches[2][0]) / 3
     h_avg_got_goals = (h_matches[0][1] + h_matches[1][1] + h_matches[2][1]) / 3
     g_avg_shot_goals = (g_matches[0][0] + g_matches[1][0] + g_matches[2][0]) / 3
     g_avg_got_goals = (g_matches[0][1] + g_matches[1][1] + g_matches[2][1]) / 3
 
-    avg_goals = ((h_avg_shot_goals, g_avg_got_goals), (h_avg_got_goals, g_avg_shot_goals))
+    avg_goals = [h_avg_shot_goals, h_avg_got_goals, g_avg_shot_goals, g_avg_got_goals]#((h_avg_shot_goals, g_avg_got_goals), (h_avg_got_goals, g_avg_shot_goals))
 
-    if outcome == -1:
-        avg_goals = ((h_avg_got_goals, g_avg_shot_goals), (h_avg_shot_goals, g_avg_got_goals))
+    # if outcome == -1:
+    #     avg_goals = ((h_avg_got_goals, g_avg_shot_goals), (h_avg_shot_goals, g_avg_got_goals))
 
-    #Estimate winning teams goals
-    for goals in avg_goals[0]:
-        if goals >= 3 and outcome != 0:
-            possibleResults[(3,0)] += 1.0
-            possibleResults[(3,1)] += 1.0
-            possibleResults[(3,2)] += 1.0
-        elif goals >= 2 and outcome == 0:
-            possibleResults[(2, 2)] += 1.0
-        else:
-            if goals >= 2:
-                possibleResults[(3,0)] += goals - int(goals)
-                possibleResults[(3,1)] += goals - int(goals)
-                possibleResults[(3,2)] += goals - int(goals)
-                possibleResults[(2,0)] += int(goals) - goals + 1
-                possibleResults[(2,1)] += int(goals) - goals + 1
-            elif goals >= 1:
-                
+    #Depending on the predicted outcome diffent results are possible
+    if outcome == 0:
+        possibleResults = {(0, 0): 0.0, (1, 1): 0.0, (2, 2): 0.0}
 
-
-    if h_shot_goals % 3 == 0:
-        for result in possibleResults:
-            if int(h_shot_goals) == result[0]:
-                possibleResults[result] += 1
+        for average in avg_goals:
+            if average >= 2:
+                possibleResults[(2, 2)] += 1.0
+            elif average > 1:
+                possibleResults[(2, 2)] += average - int(average)
+                possibleResults[(1, 1)] += int(average) + 1.0 - average
+            elif average == 1:
+                possibleResults[(1, 1)] += 1.0
+            elif average > 0:
+                possibleResults[(1, 1)] += average - int(average)
+                possibleResults[(0, 0)] += int(average) + 1.0 - average
+            else:
+                possibleResults[(0, 0)] += 1.0
     else:
-        frac_digits = h_shot_goals - int(h_shot_goals)
+        possibleResults = {(1, 0): 0.0, (2, 0): 0.0, (2, 1): 0.0, (3, 0): 0.0, (3, 1): 0.0, (3, 2): 0.0}
 
-        for result in possibleResults:
-            if int(h_shot_goals) == result[0]:
-                possibleResults[result] += int(h_shot_goals) + 1 - h_shot_goals
-            elif int(h_shot_goals) + 1 == result[0]:
-                possibleResults[result] += h_shot_goals - int(h_shot_goals)
+        if outcome == 1:
+            avg_goals = [h_avg_shot_goals, g_avg_got_goals, h_avg_got_goals, g_avg_shot_goals]
+        else:
+            avg_goals = [h_avg_got_goals, g_avg_shot_goals, h_avg_shot_goals, g_avg_got_goals]
+
+        for average in avg_goals[0:2]:
+            if average >= 3:
+                possibleResults[(3, 0)] += 1.0
+                possibleResults[(3, 1)] += 1.0
+                possibleResults[(3, 2)] += 1.0
+            elif average > 2:
+                possibleResults[(3, 0)] += average - int(average)
+                possibleResults[(3, 1)] += average - int(average)
+                possibleResults[(3, 2)] += average - int(average)
+                possibleResults[(2, 0)] += int(average) + 1.0 - average
+                possibleResults[(2, 1)] += int(average) + 1.0 - average
+            elif average == 2:
+                possibleResults[(2, 0)] += 1.0
+                possibleResults[(2, 1)] += 1.0
+            elif average > 1:
+                possibleResults[(2, 0)] += average - int(average)
+                possibleResults[(2, 1)] += average - int(average)
+                possibleResults[(1, 0)] += int(average) + 1.0 - average
+            elif average == 1:
+                possibleResults[(1, 0)] += 1.0
+            elif average > 0:
+                possibleResults[(1, 0)] += average - int(average)
+            else:
+                possibleResults[(1, 0)] += 1.0
+
+        for average in avg_goals[2:4]:
+            if average >= 2:
+                possibleResults[(3, 2)] += 1.0
+            elif average > 1:
+                possibleResults[(3, 2)] += average - int(average)
+                possibleResults[(3, 1)] += int(average) + 1.0 - average
+                possibleResults[(2, 1)] += int(average) + 1.0 - average
+            elif average == 1:
+                possibleResults[(3, 1)] += 1.0
+                possibleResults[(2, 1)] += 1.0
+            elif average > 0:
+                possibleResults[(3, 1)] += average - int(average)
+                possibleResults[(2, 1)] += average - int(average)
+                possibleResults[(3, 0)] += int(average) + 1.0 - average
+                possibleResults[(2, 0)] += int(average) + 1.0 - average
+                possibleResults[(1, 0)] += int(average) + 1.0 - average
+            else:
+                possibleResults[(3, 0)] += 1.0
+                possibleResults[(2, 0)] += 1.0
+                possibleResults[(1, 0)] += 1.0
+
+    max(possibleResults.items(), )
+    if outcome == 1:
+        return
+
+                # for i in range(4, -1):
+    #     for j in range(2):
+    #         if avg_goals[0][j] > i:
+    #             for res in possibleResults:
+    #                 if res[0] == i and i == 3:
+    #                     possibleResults[res] += 1.0
+    #                 elif res[0] == i:
+    #                     possibleResults[res] +=
+    #                 elif res[0] == i + 1:
+    #                     possibleResults
+    #         elif avg_goals[0][j] == i:
+    #
+    # #Estimate winning teams goals
+    # for goals in avg_goals[0]:
+    #     if goals >= 3 and outcome != 0:
+    #         possibleResults[(3,0)] += 1.0
+    #         possibleResults[(3,1)] += 1.0
+    #         possibleResults[(3,2)] += 1.0
+    #     elif goals >= 2 and outcome == 0:
+    #         possibleResults[(2, 2)] += 1.0
+    #     else:
+    #         if goals >= 2:
+    #             possibleResults[(3,0)] += goals - int(goals)
+    #             possibleResults[(3,1)] += goals - int(goals)
+    #             possibleResults[(3,2)] += goals - int(goals)
+    #             possibleResults[(2,0)] += int(goals) - goals + 1
+    #             possibleResults[(2,1)] += int(goals) - goals + 1
+    #         elif goals >= 1:
+    #
+    #
+    #
+    # if h_shot_goals % 3 == 0:
+    #     for result in possibleResults:
+    #         if int(h_shot_goals) == result[0]:
+    #             possibleResults[result] += 1
+    # else:
+    #     frac_digits = h_shot_goals - int(h_shot_goals)
+    #
+    #     for result in possibleResults:
+    #         if int(h_shot_goals) == result[0]:
+    #             possibleResults[result] += int(h_shot_goals) + 1 - h_shot_goals
+    #         elif int(h_shot_goals) + 1 == result[0]:
+    #             possibleResults[result] += h_shot_goals - int(h_shot_goals)
 
 if __name__ == '__main__':
     #We need two teams that play against each other (Later the names of the teams are given as an input to the program.
@@ -140,10 +221,3 @@ if __name__ == '__main__':
     h_matches = ((0,2), (0,2), (0,2))
     g_matches = ((0,2), (0,2), (0,2))
 
-    for i in (0,2), (0,1), (0,0), (1,0), (2,0):
-        for j in (0, 2), (0, 1), (0, 0), (1, 0), (2, 0):
-            for k in (0, 2), (0, 1), (0, 0), (1, 0), (2, 0):
-                for l in (0, 2), (0, 1), (0, 0), (1, 0), (2, 0):
-                    for m in (0, 2), (0, 1), (0, 0), (1, 0), (2, 0):
-                        for n in (0, 2), (0, 1), (0, 0), (1, 0), (2, 0):
-                            print((i,j,k), ",", (l,m,n), ": ", compare_runs((i,j,k), (l,m,n)))
